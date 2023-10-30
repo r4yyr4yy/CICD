6110ce3ade8791045677880b057f8b94d66df655
Raymond Okudzeto <123817776+r4yyr4yy@users.noreply.github.com>
2023-10-30 16:10:00 +0000

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,8 +2,11 @@
 set -e
 
 # Pull the Docker image from Docker hub
-# docker pull rayray/demo-app
-echo "Starting ... "
+
+echo "Pull Docker iamge from Docker hub"
+
+docker pull raymondokudzeto/tourist-app
 
 # Run the Docker image as a container
-# docker run  -d  -p 5000:5000 CICD/
+
+docker run  -d  -p 5000:5000 raymondokudzeto/tourist-app

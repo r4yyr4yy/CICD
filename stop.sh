4b553c1c6717916842f80890d667439dd32f353b
Raymond Okudzeto <123817776+r4yyr4yy@users.noreply.github.com>
2023-11-12 12:35:35 +0000

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+set -x
 
 # stop the running container (if any)
 
-containerid='docker ps | awk -F " " '{print $1}''
+containerid=$(docker ps | awk -F " " '{print $1}')
 
 docker rm  -f $containerid
 
